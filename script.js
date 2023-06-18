ee3f8a802b59803507abad3aece7b7cb29f17abf
Hasan Okcu <okcuhhasan@gmail.com>
2023-06-18 14:16:58 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+var searchValue = document.querySelector("#searchbox");
+
+searchValue.addEventListener("keypress",setFunc);
+
+function setFunc(e){
+    if(e.keyCode == 13){
+        getData(searchValue.value)
+    }
+}
+
+function getData(value){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+value+'&appid=672fbee1c5a0cd0266c5dbab9753f661')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (weather){
+        document.querySelector('#city').innerHTML = weather.name;
+        document.querySelector('#temp').innerHTML = weather.main.temp;
+        document.querySelector('#min_max_temp').innerHTML = 
+        "Min. "+weather.main.temp_min+" Max. "+weather.main.temp_max;
+        document.querySelector('#weather').innerHTML = weather.weather[0].main;
+        document.querySelector('#weather_desc').innerHTML = weather.weather[0].description;
+        document.querySelector('#wind_dir_speed').innerHTML = "Wind direction: " + weather.wind.deg
+        + " / Wind speed: "+weather.wind.speed;
+    })
+    .catch(function (err){
+        console.log(err);
+    })
+}
+
